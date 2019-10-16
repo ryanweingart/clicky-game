d6dac1844cf117c71df65effe7e751c66cacbbaf
Ryan Weingart <rmweingart5@gmail.com>
2019-10-16 17:06:51 -0400

    added imports to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Cards from "./components/Cards";
+import Header from "./components/Header";
+import Title from "./components/Title";
+import Container from "./components/Container";
+import images from "./images.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
 
 export default App;
