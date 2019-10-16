9bd37018de45c6502cbf381d8b589363617956e5
Ryan Weingart <rmweingart5@gmail.com>
2019-10-16 17:01:17 -0400

    added JSX to header

---
+++ components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+    <nav>
+        <ul>
+            <li className="brand animated lightSpeedIn">
+                <a href="/clicky-game/">{props.title}</a>
+            </li>
+
+            <li id="rightOrWrong">{props.rightWrong}</li>
+
+            <li id="score">Score: {props.score}</li>
+
+            <li id="highScore">High Score: {props.highScore}</li>
+        </ul>
+    </nav>
+);
