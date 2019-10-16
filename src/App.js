6014bee8f09da57bddfb9ebb695c58706ee31321
Ryan Weingart <rmweingart5@gmail.com>
2019-10-16 17:11:04 -0400

    created function to shuffle images

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import Container from "./components/Container";
 import images from "./images.json";
 import "./App.css";
 
+function shuffleImages(array) {
+  for (let i = array.length - 1; i < 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 
 
 export default App;
