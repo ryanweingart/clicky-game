daaec893c03bc96cad778c5aa3945cb5f057095b
Ryan Weingart <rmweingart5@gmail.com>
2019-10-18 17:08:38 -0400

    Updates

---
+++ precache-manifest.8de765bf69b2851ac698473b1c9b4fb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3beb604e14a667b3154bbae16ffd9a6e",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1d88f3c4ef3407a7f580",
+    "url": "/clicky-game/static/css/main.1227d03e.chunk.css"
+  },
+  {
+    "revision": "20fcd77cc4ad2f356b72",
+    "url": "/clicky-game/static/js/2.a3498d0b.chunk.js"
+  },
+  {
+    "revision": "1d88f3c4ef3407a7f580",
+    "url": "/clicky-game/static/js/main.c84e38e0.chunk.js"
+  },
+  {
+    "revision": "6e3658582142cd6ca0be",
+    "url": "/clicky-game/static/js/runtime-main.5997d276.js"
+  }
+]);
